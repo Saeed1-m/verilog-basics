9a1ead1784ae0379d3c260fe742644e9447c0625
Saeed <83759239+Saeed1-m@users.noreply.github.com>
2025-10-06 23:54:30 +0330

    Update and_gate.v

--- gates/and_gate.v
+++ gates/and_gate.v
@@ -5,3 +5,22 @@ module and_gate(
 );
 assign y = a & b;
 endmodule
+
+
+
+`timescale 1ns / 1ps
+module and_gate_tb;
+    reg a, b;
+    wire y;
+    and_gate uut(.a(a), .b(b), .y(y));
+
+    initial begin
+        $display("AND Gate Test");
+        $display("a b | y");
+        a=0; b=0; #10 $display("%b %b | %b", a,b,y);
+        a=0; b=1; #10 $display("%b %b | %b", a,b,y);
+        a=1; b=0; #10 $display("%b %b | %b", a,b,y);
+        a=1; b=1; #10 $display("%b %b | %b", a,b,y);
+        $finish;
+    end
+endmodule
